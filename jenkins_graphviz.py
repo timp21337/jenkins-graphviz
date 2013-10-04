96ac140b9c912e2448b3246b5431f8acc2fcc3b7
timp21337 <timp@paneris.org>
2013-10-04 16:24:34 +0100

    Add access to password protected jenkins

--- jenkins_graphviz.py
+++ jenkins_graphviz.py
@@ -1,8 +1,9 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 
 from __future__ import print_function
 
 import argparse
+import base64
 import itertools
 import json
 import string
@@ -13,20 +14,30 @@ import sys
 import lxml.objectify
 
 dot_template = '''
-digraph {
-    graph [rankdir="LR",fontsize=9,fontname="Sans"]
+digraph {    
+    graph [rankdir="LR",fontsize=16,fontname="Sans"]
+    labelloc=top;
+    label="Jobs on $server";
+
+
     node [fontname="Sans",fontsize=9]
 
-    $repos
-    $trigger_edges
+// repos
+$repos
+
+// trigger edges
+$trigger_edges
 
     subgraph cluster_view {
         label = "$view_name"
-        $view_jobs
+// view jobs
+$view_jobs
     }
+
     $other_jobs
     $subproject_edges
     $pipeline_edges
+
 }
 '''
 
@@ -44,22 +55,40 @@ def view_url(base, view):
     '''
     return urlparse.urljoin(base, 'view/{0}/'.format(urllib.quote(view)) if view else '')
 
-def http_fetch(url):
+def http_fetch(url, username=None, password=None):
     import urllib2
+    request = urllib2.Request(url, None)
+    if username is not None: 
+        base64string = base64.encodestring('%s:%s' % (username, password)).replace('\n', '')
+        request.add_header("Authorization", "Basic %s" % base64string)
     try:
-        return urllib2.urlopen(url)
+        return urllib2.urlopen(request)
     except:
         print('While fetching <{0}>:'.format(url), file=sys.stderr)
+        if username is not None:
+        	print('Using username <{0}>'.format(username), file=sys.stderr)
         raise
 
-def api_fetch(url):
+def api_fetch(url, username=None, password=None):
     url = urlparse.urljoin(url, 'api/json')
-    return json.load(http_fetch(url))
+    return json.load(http_fetch(url, username, password))
+
+
+def fix_job(job): 
+    if job['color'] == 'disabled':
+        job['color'] = 'grey'
+    if job['color'] == 'blue_anime':
+        job['color'] = 'blue'
+    return job
+
 
 def main():
     parser = argparse.ArgumentParser(description='Output a Graphviz graph based on relationships between Jenkins jobs')
     parser.add_argument('server', help='URL of Jenkins server')
     parser.add_argument('--view', '-v', help='Filter jobs by view')
+    parser.add_argument('--username', '-u', help='Jenkins username')
+    parser.add_argument('--password', '-p', help='Jenkins password')
+
     args = parser.parse_args()
 
     view_jobs = {}
@@ -69,27 +98,30 @@ def main():
     repos = set()
     trigger_edges = set()
 
-    url = view_url('http://hades:8081/', args.view)
-    for job in api_fetch(url)['jobs']:
-        view_jobs[job['name']] = job
+    url = view_url(args.server, args.view)
+    for job in api_fetch(url, args.username, args.password)['jobs']:
+        view_jobs[job['name']] = fix_job(job)
 
     # Downstreams are when a job uses the 'build other projects' post-build action
     for job in view_jobs.values():
-        job_detail = api_fetch(job['url'])
+        job_detail = api_fetch(job['url'], args.username, args.password)
         for downstream in job_detail['downstreamProjects']:
             pipeline_edges.add ((job['name'], downstream['name']))
             if downstream['name'] not in view_jobs:
-                other_jobs[downstream['name']] = downstream
+                other_jobs[downstream['name']] = fix_job(downstream)
         for upstream in job_detail['upstreamProjects']:
             pipeline_edges.add ((upstream['name'], job['name']))
             if upstream['name'] not in view_jobs:
-                other_jobs[upstream['name']] = upstream
+                other_jobs[upstream['name']] = fix_job(upstream)
 
     for job in itertools.chain(view_jobs.values(), other_jobs.values()):
-        job['config'] = lxml.objectify.parse(http_fetch(urlparse.urljoin(job['url'], 'config.xml')))
+        job['config'] = lxml.objectify.parse(
+            http_fetch(urlparse.urljoin(job['url'], 'config.xml'), args.username, args.password))
 
         job['subprojects'] = set()
-        subprojects = job['config'].xpath('/*/builders/hudson.plugins.parameterizedtrigger.TriggerBuilder/configs/hudson.plugins.parameterizedtrigger.BlockableBuildTriggerConfig/projects')
+        subprojects = job['config'].xpath(
+            '/*/builders/hudson.plugins.parameterizedtrigger.TriggerBuilder/' + 
+            'configs/hudson.plugins.parameterizedtrigger.BlockableBuildTriggerConfig/projects')
         if subprojects:
             job['subprojects'].update([s.strip() for s in str(subprojects[0]).split(',')])
 
@@ -102,18 +134,31 @@ def main():
         for repo in job['config'].xpath('/*/scm/userRemoteConfigs/hudson.plugins.git.UserRemoteConfig'):
             for branch in job['config'].xpath('/*/scm/branches/hudson.plugins.git.BranchSpec'):
                 for trigger in job['config'].xpath('/*/triggers/*'):
-                    if trigger.tag in ['hudson.triggers.TimerTrigger', 'hudson.triggers.SCMTrigger', 'com.cloudbees.jenkins.GitHubPushTrigger']:
-                        repos.add((repo.url, branch.name))
-                        trigger_edges.add(((repo.url, branch.name), job['name']))
+                    if trigger.tag in ['hudson.triggers.TimerTrigger', 
+                                       'hudson.triggers.SCMTrigger', 
+                                       'com.cloudbees.jenkins.GitHubPushTrigger']:
+                        repos.add((str(repo.url).rpartition('/')[2], branch.name))
+                        trigger_edges.add((((str(repo.url).rpartition('/')[2], branch.name)), job['name']))
                         break
                     print('Unknown trigger: {}', file=sys.stderr)
 
     print(string.Template(dot_template).substitute({
-        'repos': '\n'.join(['"{0}\\n{1}" [URL="{2}",shape=tab]'.format(repo[0], repo[1], str(repo[0]).replace('git@github.com:', 'https://github.com/', 1)) for repo in repos]),
-        'trigger_edges': '\n'.join(['"{0}\\n{1}" -> "{2}"'.format(repo[0], repo[1], job) for repo, job in trigger_edges]),
+        'repos': '\n'.join(
+                    ['"{0}\\n{1}" [URL="{2}",shape=oval]'.format(
+                        repo[0], repo[1], str(repo[0]).replace('git@github.com:', '', 1)) for repo in repos]),
+        'server': args.server,
         'view_name': args.view if args.view else '',
-        'view_jobs': '\n'.join(['"{0}" [shape="box", URL="{1}", color="{2}", fontcolor="{2}"]'.format(job['name'], job['url'], 'black' if job['enabled'] else 'grey') for name, job in sorted(view_jobs.iteritems())]),
-        'other_jobs': '\n'.join(['"{0}" [shape="box", URL="{1}"]'.format(job['name'], job['url']) for name, job in sorted(other_jobs.iteritems())]),
+        'trigger_edges': '\n'.join(['"{0}\\n{1}" -> "{2}"'.format(repo[0], repo[1], job) for repo, job in trigger_edges]),
+        'view_jobs': '\n'.join(['"{0}" [shape="box", URL="{1}", color="{2}", fontcolor="{3}"]'.format(
+            job['name'], 
+            job['url'], 
+            job['color'], 
+            'black' if job['enabled'] else 'grey'
+        ) for name, job in sorted(view_jobs.iteritems())]),
+        'other_jobs': '\n'.join(['"{0}" [shape="box", URL="{1}", color="{2}"]'.format(
+            job['name'], 
+            job['url'], 
+            job['color']) for name, job in sorted(other_jobs.iteritems())]),
         'pipeline_edges': '\n'.join(['"{0}" -> "{1}"'.format(a, b) for a, b in pipeline_edges]),
         'subproject_edges': '\n'.join(['"{0}" -> "{1}" [style=dotted]'.format(a, b) for a, b in subproject_edges])
     }))
@@ -121,4 +166,4 @@ def main():
 if __name__ == '__main__':
     main()
 
-# vim: ts=8 sts=4 sw=4 et
+# vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4
